22979fa6965ee09758a77caf03650da4fd847d0e
Buff4eyes <claudemokbel@gmail.com>
2022-01-14 18:48:29 -0800

    userRoutes created and tested

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User.js');
 const Thought = require('./Thought.js');
-const Reaction = require('./Reaction.js')
+const Reaction = require('./Reaction.js');
+
+module.exports = {
+  User,
+  Thought,
+  Reaction
+}
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,47 @@ const router = require('express').Router()
 const { User } = require('../../models')
 
 //Register New User
-router.post('/users', async function (req,res) {
+router.post('/', async function (req,res) {
  const user = await User.create(req.body);
  res.json(user);
+});
+
+//get users
+router.get('/', async function (req,res) {
+  const users = await User.find({}).populate('thoughts');
+  res.json(users);
+});
+
+//get one user
+router.get('/:id', async function (req, res) {
+  const user = await User.findById(req.params.id).populate('thoughts');
+  res.json(user);
+});
+
+//update one users
+router.put('/:id', async function (req,res) {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  res.sendStatus(200);
+});
+
+//delete a user
+router.delete('/:id', async function  (req,res) {
+  await User.findByIdAndDelete(req.params.id);
+  res.sendStatus(200);
+});
+
+// add a friend
+router.post('/:userId/friend/:friendId', async function(req,res) {
+  const friend = await User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId } });
+  const friend2 = await User.findByIdAndUpdate(req.params.friendId, { $addToSet: { friends: req.params.userId } });
+  res.sendStatus(200);
+});
+
+// delete a friend
+router.delete('/:userId/friend/:friendId', async function (req, res) {
+  const friend = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId } });
+  const friend2 = await User.findByIdAndUpdate(req.params.friendId, { $pull: { friends: req.params.userId } });
+  res.sendStatus(200);
 });
 
 module.exports = router
