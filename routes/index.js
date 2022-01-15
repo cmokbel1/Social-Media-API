a0bb512676a872fa286c4cdf5685606d2f5e3544
Buff4eyes <claudemokbel@gmail.com>
2022-01-14 17:13:23 -0800

    commentary

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
+// calling express router
 const router = require('express').Router();
+// defining api routes
 const apiRoutes = require('./api');
 
+// telling the router to use the /api route from the apiRoutes const defined above
 router.use('/api', apiRoutes)
 
+// error catch for wrong route
 router.use((req,res) => {
   return res.send('Wong Route!');
 });
 
+//export router
 module.exports = router;
