621d22aebece9f76270fd5115e4577764e019933
Buff4eyes <claudemokbel@gmail.com>
2022-01-14 17:00:54 -0800

    added some code comments

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,22 +1,26 @@
 // import Schema build from mongoose NPM
 const { Schema, model } = require('mongoose');
 
+//Schema built for reactions
 const reactionSchema = new Schema({
   reactions: {
     type: String,
     required: true,
     maxlength: 300
   },
+  // reactions reference specific User
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+  // reactions reference specific thought
   thought: {
     type: Schema.Types.ObjectId,
     ref: 'Thought',
     required: true
   }
+  // time stamps allow reactions to be stamped with date and time / may need conversion to local time
 }, { timestamps: true });
 
 const Reaction = model('Reaction', reactionSchema);
